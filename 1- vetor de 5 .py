b10b9045e1adb5ffcc552c7d93caa19dae04837b
NIGER <52137118+NIGEROLIVEIRA@users.noreply.github.com>
2020-01-10 10:53:01 -0400

    Delete 1- vetor de 5 .py

--- 1- vetor de 5 .py
+++
@@ -1,9 +0,0 @@
-def entrada():
-    vetor = []
-    for x in range(1, 6):
-        numero = int(input('Digite o {0}° numero inteiro: '.format(x)))
-        vetor.append(numero)
-    return vetor
-
-vetor = entrada()
-print('vetor de [5] = ', vetor)
